8e94da0187ac9bb9839912047c54383c2cd58c05
CraSal <57324195+CraSal@users.noreply.github.com>
2020-02-23 14:00:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -11,15 +11,10 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
     
 @bot.command()
-async def neko(ctx):
-    await ctx.send('nya-n')
+async def sensi(ctx):
+    await ctx.send('他ゲームとのマウスの感度を揃えたいならココ！(※一部課金必※)\nhttps://www.mouse-sensitivity.com/')
 
 
 bot.run(token)
